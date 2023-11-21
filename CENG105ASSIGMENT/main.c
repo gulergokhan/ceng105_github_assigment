f84483057cd6417e98354253c28a66a86344f57e
Gökhan Güler <151137955+gulergokhan@users.noreply.github.com>
2023-11-21 20:19:17 +0300

    Update main.c

--- CENG105ASSIGMENT/main.c
+++ CENG105ASSIGMENT/main.c
@@ -5,4 +5,6 @@ int main(void)
     printf("enter the temperature degree of today:");
     scanf("%d",&temp);
     if(temp<=4)
-	printf("the weather is cold")
+	printf("the weather is cold");
+  else if(temp>=25)
+    printf("the weather is hot");
